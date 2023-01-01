decd8664395e3535baf78430e2187f64f5cd43c4
YOOJS1205 <junsang8921@gmail.com>
2023-01-01 17:25:54 +0900

    Fix: index.tsx 테스트 코드 삭제

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,4 @@
 import Head from 'next/head';
-import BigButton from '../components/common/BigButton';
-import SmallButton from '../components/common/SmallButton';
-import UserInput from '../components/common/UserInput';
 
 function Home() {
   return (
@@ -9,10 +6,6 @@ function Home() {
       <Head>
         <title>JustPay-dev</title>
       </Head>
-      <BigButton text="다음" isDisabled={false} onClick={() => console.log('hi')} />
-      <SmallButton text="버튼" isDisabled={true} />
-      <SmallButton text="버튼" isDisabled={false} />
-      <UserInput placeholder="정확한 상품 개수를 입력해주세요" inputTextGuide="개" />
     </div>
   );
 }
