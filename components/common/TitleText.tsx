006a3469c903cf65e4d9e4a0843864b7e5b8d75a
wooyoung0830 <thddndud0830@gmail.com>
2023-01-02 00:13:57 +0900

    refact : 불필요한 import문 삭제

--- components/common/TitleText.tsx
+++ components/common/TitleText.tsx
@@ -1,7 +1,5 @@
 import React, { Children, ReactNode } from 'react';
 import styled from 'styled-components';
-import MainText from './MainText';
-import SubText from './SubText';
 
 interface TitleTextProps {
   children: ReactNode;
