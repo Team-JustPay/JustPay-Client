46332502185212c90cf63f52cad62b7e7e9ba35a
wooyoung0830 <thddndud0830@gmail.com>
2023-01-02 19:20:01 +0900

    feat : sell layout 적용

--- pages/sell/qcGuide.tsx
+++ pages/sell/qcGuide.tsx
@@ -11,28 +11,31 @@ import ContentsContainer from 'components/sell/guide/ContentsContainer';
 import { GuideText } from 'components/sell/guide/GuideText';
 import FirstQCGuidePic from '../../public/assets/icons/qcGuide1.svg';
 import BigButton from 'components/common/BigButton';
+import layout from './layout';
 
 export default function qcGuide() {
   return (
-    <Root>
-      <Header title={TITLE.ADD_SELLPOST} rightButtonText={MENU.CANCEL} isHavingBackButton />
-      <TitleText>
-        <MainText text={'인증 전용 사진을 추가로 등록해주세요'}></MainText>
-        <SubText text="매칭된 구매자에게만 보일 인증 사진을 등록해주세요"></SubText>
-      </TitleText>
-      <GuideContainer>
-        <Progress stage={1} />
-        <ContentsContainer>
-          <FirstQCGuidePic />
-          <GuideText>
-            자신의 아이디와 인증단어를 적어 상품과 함께 촬영해주세요 구매자가 인증사진을 확인하고 결제하면 간편하게 거래
-            완료!
-          </GuideText>
-        </ContentsContainer>
-      </GuideContainer>
+    <>
+      <div>
+        <Header title={TITLE.ADD_SELLPOST} rightButtonText={MENU.CANCEL} isHavingBackButton />
+        <TitleText>
+          <MainText text={'인증 전용 사진을 추가로 등록해주세요'}></MainText>
+          <SubText text="매칭된 구매자에게만 보일 인증 사진을 등록해주세요"></SubText>
+        </TitleText>
+        <GuideContainer>
+          <Progress stage={1} />
+          <ContentsContainer>
+            <FirstQCGuidePic />
+            <GuideText>
+              자신의 아이디와 인증단어를 적어 상품과 함께 촬영해주세요 구매자가 인증사진을 확인하고 결제하면 간편하게
+              거래 완료!
+            </GuideText>
+          </ContentsContainer>
+        </GuideContainer>
+      </div>
       <BigButton text="다음" isDisabled={false} onClick={() => {}} />
-    </Root>
+    </>
   );
 }
 
-const Root = styled.div``;
+qcGuide.Layout = layout;
