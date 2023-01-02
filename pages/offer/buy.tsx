3cde4bdf14d60b93a3350f4b2dd5fba89f15914f
1 <wrasf175@naver.com>
2023-01-02 16:57:04 +0900

    refactor: 스타일 초기세팅, 이미지 업로드 버튼 크기 수정

--- pages/offer/buy.tsx
+++ pages/offer/buy.tsx
@@ -13,9 +13,8 @@ import SmallButton from 'components/common/SmallButton';
 export default function buy() {
   return (
     <Root>
-      {/* 지정가 + 일괄 */}
       <Header title={TITLE.OFFER_TO_SELLER} rightButtonText={MENU.BACK} isHavingBackButton />
-      <ImagePostButton buttonSize="big" />
+      <ImagePostButton buttonSize="small" />
       <MainText text="구매하는 가격을 확인하세요" />
       <UserInput placeholder="500원 단위로 입력해주세요" inputTextGuide="원" />
       <MainText text="배송가능 옵션 을 1가지만 선택하세요" />
@@ -25,8 +24,7 @@ export default function buy() {
       <UserInput placeholder="500원 단위로 입력해주세요" inputTextGuide="원" />
       <UserInput placeholder="500원 단위로 입력해주세요" inputTextGuide="원" />
 
-      {/* 제시가능 + 일괄 */}
-      {/* <TitleText>
+      <TitleText>
         <MainText text="구매 희망 상품을 표시한 사진을 등록해주세요" />
         <SubText text="일괄 구매하는 경우에는 표시하지 않아도 돼요" />
       </TitleText>
@@ -40,11 +38,9 @@ export default function buy() {
 
       <MainText text="구매 희망가격을 제시하세요"></MainText>
       <UserInput placeholder="500원 단위로 입력해주세요" inputTextGuide="원" />
-      <MainText text="배송가능 옵션 을 1가지만 선택하세요" /> */}
+      <MainText text="배송가능 옵션 을 1가지만 선택하세요" />
 
-      {/* 제시가능 + 일부 */}
-
-      {/* <TitleText>
+      <TitleText>
         <MainText text="구매 희망 상품을 표시한 사진을 등록해주세요" />
         <SubText text="일괄 구매하는 경우에는 표시하지 않아도 돼요" />
       </TitleText>
@@ -69,7 +65,7 @@ export default function buy() {
 
       <MainText text="구매 희망가격을 제시하세요"></MainText>
       <UserInput placeholder="500원 단위로 입력해주세요" inputTextGuide="원" />
-      <MainText text="배송가능 옵션 을 1가지만 선택하세요" /> */}
+      <MainText text="배송가능 옵션 을 1가지만 선택하세요" />
 
       <BigButton text="다음" isDisabled={true} onClick={() => {}} />
     </Root>
