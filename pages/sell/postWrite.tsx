91bbc9834c1f04a1872aa106bf6fe67d37d7dfbb
wooyoung0830 <thddndud0830@gmail.com>
2023-01-03 13:48:37 +0900

    feat : 링크 아이콘 클릭시 토스트 메시지

--- pages/sell/postWrite.tsx
+++ pages/sell/postWrite.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainText from 'components/common/MainText';
 import SubText from 'components/common/SubText';
 import BigButton from 'components/common/BigButton';
@@ -9,7 +9,13 @@ import LinkIcon from '../../public/assets/icons/linkIcon.svg';
 import TwitterIcon from '../../public/assets/icons/twitterIcon.svg';
 import CompletePostWrite from '../../public/assets/icons/completePostWrite.svg';
 export default function postWrite() {
-  const copyLink = () => {};
+  const [isCopyLink, setIsCopyLink] = useState(false);
+  const copyLink = () => {
+    setIsCopyLink(true);
+    setTimeout(() => {
+      setIsCopyLink(false);
+    }, 2000);
+  };
 
   const uploadOnTwitter = () => {};
   return (
@@ -28,7 +34,10 @@ export default function postWrite() {
           <CompletePostWrite />
         </StyledCompleteIcon>
       </div>
-      <BigButton text="판매글 본문 보기" isDisabled={false} onClick={() => {}} />
+      <StyledButtomConatiner>
+        {isCopyLink && <StyledCopyLinkToastMessage>클립보드에 복사되었어요</StyledCopyLinkToastMessage>}
+        <BigButton text="판매글 본문 보기" isDisabled={false} onClick={() => {}} />
+      </StyledButtomConatiner>
     </>
   );
 }
@@ -59,4 +68,25 @@ const StyledIconContainer = styled.section`
 const StyledCompleteIcon = styled.section`
   display: flex;
   justify-content: center;
+`;
+
+const StyledCopyLinkToastMessage = styled.article`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 17.7rem;
+  height: 3.8rem;
+  margin-bottom: 2rem;
+
+  color: ${({ theme }) => theme.colors.white};
+  background-color: ${({ theme }) => theme.colors.gray1};
+  border-radius: 0.8rem;
+  ${({ theme }) => theme.fonts.medium14pt};
+`;
+
+const StyledButtomConatiner = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
