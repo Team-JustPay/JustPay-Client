4ccc5721f7ce62e0a51eda62181d3fd6e548f450
YOOJS1205 <junsang8921@gmail.com>
2023-01-07 04:43:01 +0900

    Feat: 매칭중 페이지 토스트 메세지 생성 및 토스트 메세지 스타일 수정

--- pages/matching/[id].tsx
+++ pages/matching/[id].tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
 
 import Header from 'components/matching/Header';
 import PriceInfo from 'components/matching/PriceInfo';
@@ -12,10 +11,13 @@ import ItemContainer from 'components/matching/ItemContainer';
 import SuggestItem from 'components/matching/SuggestItem';
 import BigButton from 'components/common/BigButton';
 import Modal from 'components/common/Modal';
+import ToastMessage from 'components/common/ToastMessage';
 
 export default function matching() {
   const [isClicked, setIsClicked] = useState(true);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isMine, setIsMine] = useState(false);
+  const [isSuggested, setIsSuggested] = useState(false);
 
   const handleOptionTab = () => {
     setIsClicked((prev) => !prev);
@@ -29,9 +31,16 @@ export default function matching() {
     setIsModalOpen(false);
   };
 
+  const handleSuggetButton = () => {
+    setIsSuggested(true);
+    setTimeout(() => {
+      setIsSuggested(false);
+    }, 2000);
+  };
+
   return (
-    <div>
-      <Header isMine={true} modalOpenFunc={handleClickCancelButton} />
+    <>
+      <Header isMine={isMine} modalOpenFunc={handleClickCancelButton} />
       <PriceInfo highestPrice={100000} />
       <UserProfile profileImage="" userName="스윙스" userId="@King_swings" />
       <SaleInfoContainer productCount={3} salesOption="일괄 판매만" priceOption="지정가격" />
@@ -48,7 +57,8 @@ export default function matching() {
           <SuggestItem itemSize="small" description="매칭 대기중" />
         </ItemContainer>
       </SuggestContainer>
-      <BigButton text="다음" isDisabled={false} />
+      {isSuggested && <ToastMessage text="판매글에 구매를 제시했어요!" />}
+      {!isMine && <BigButton text="구매 제시하기" isDisabled={false} onClick={handleSuggetButton} />}
       {isModalOpen && (
         <Modal
           title="판매를 종료하시겠어요?"
@@ -59,6 +69,6 @@ export default function matching() {
           buttonFirstFunction={handleClickCloseButton}
         />
       )}
-    </div>
+    </>
   );
 }
