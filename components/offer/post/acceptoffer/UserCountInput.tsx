1e889b392e9b53a0cb30d514309d77b73c28a249
1 <wrasf175@naver.com>
2023-01-07 23:01:40 +0900

    fix: subText 공통컴포넌트에 들어가는 props 명시

---
+++ components/offer/post/acceptoffer/UserCountInput.tsx
@@ -0,0 +1,96 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+
+interface InputProps {
+  placeholder: string;
+  inputTextGuide: string;
+  onChangeFunc: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  inputText: string;
+  countOverCheck: boolean;
+}
+
+interface StyledInputProps {
+  countOverCheck: boolean;
+}
+
+interface InputTextProps {
+  inputText: string;
+  countOverCheck: boolean;
+}
+
+export default function UserCountInput({
+  placeholder,
+  inputTextGuide,
+  onChangeFunc,
+  inputText,
+  countOverCheck,
+}: InputProps) {
+  return (
+    <InputContainer>
+      <StyledInput
+        countOverCheck={countOverCheck}
+        placeholder={placeholder}
+        onChange={onChangeFunc}
+        value={inputText}
+        type="text"
+      />
+      <InputText inputText={inputText} countOverCheck={countOverCheck}>
+        {countOverCheck ? '5일 까지 입력할 수 있어요' : inputTextGuide}
+      </InputText>
+    </InputContainer>
+  );
+}
+
+const InputContainer = styled.article`
+  position: relative;
+`;
+
+const StyledInput = styled.input<StyledInputProps>`
+  width: 100%;
+  padding: 2rem;
+
+  color: ${({ theme, countOverCheck }) => (countOverCheck ? theme.colors.sub2 : theme.colors.main)};
+  border: 0.1rem solid;
+  border-color: ${({ theme }) => theme.colors.gray3};
+  border-radius: 0.8rem;
+  background-color: ${({ theme }) => theme.colors.gray_background};
+  font-weight: 700;
+  font-size: 1.6rem;
+  line-height: 1.9rem;
+
+  &:focus {
+    border: 0.2rem solid;
+    border-color: ${({ theme, countOverCheck }) => (countOverCheck ? theme.colors.sub2 : theme.colors.main)};
+    & + strong {
+      font-weight: 700;
+    }
+  }
+
+  &::placeholder {
+    font-weight: 400;
+  }
+`;
+
+const InputText = styled.strong<InputTextProps>`
+  position: absolute;
+  top: 2rem;
+  right: 2rem;
+
+  color: ${({ inputText }) => (inputText ? ({ theme }) => theme.colors.main : ({ theme }) => theme.colors.gray1)};
+
+  font-weight: 400;
+  font-size: 1.6rem;
+  line-height: 1.9rem;
+  ${({ countOverCheck }) =>
+    countOverCheck
+      ? css`
+          font-size: 1.2rem;
+          line-height: 1.4rem;
+          margin-top: 0.4rem;
+          color: ${({ theme }) => theme.colors.sub2};
+        `
+      : css`
+          font-size: 1.6rem;
+          line-height: 1.9rem;
+        `}
+`;
