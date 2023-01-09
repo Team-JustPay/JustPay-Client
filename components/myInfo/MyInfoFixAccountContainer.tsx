9678e80d6068f05d447ce3ad2a8cc83565c311c1
wooyoung0830 <thddndud0830@gmail.com>
2023-01-09 18:46:12 +0900

    feat : 안전 결제 정보 탭

--- components/myInfo/MyInfoFixAccountContainer.tsx
+++ components/myInfo/MyInfoFixAccountContainer.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import MyInfoItem from './MyInfoItem';
+import MyInfoInput from './MyInfoInput';
 
 interface MyInfoAccountContainerProps {
   accountNumber: string;
@@ -8,7 +8,15 @@ interface MyInfoAccountContainerProps {
 export default function MyInfoFixAccountContainer() {
   return (
     <>
-      <StyledMyInfoContainer>계정 수정</StyledMyInfoContainer>
+      <StyledMyInfoContainer>
+        입금자명 <strong>*</strong> <MyInfoInput placehoderText="입금하는 사람 이름을 입력하세요" />
+      </StyledMyInfoContainer>
+      <StyledMyInfoContainer>
+        은행명 <strong>*</strong> <MyInfoInput placehoderText="은행 이름을 입력하세요 (ex. 우리은행)" />
+      </StyledMyInfoContainer>
+      <StyledMyInfoContainer>
+        계좌번호 <strong>*</strong> <MyInfoInput placehoderText="계좌번호를 입력하세요 (- 제외)" />
+      </StyledMyInfoContainer>
     </>
   );
 }
@@ -16,22 +24,9 @@ export default function MyInfoFixAccountContainer() {
 const StyledMyInfoContainer = styled.section`
   margin-bottom: 4rem;
   color: ${({ theme }) => theme.colors.white};
-  ${({ theme }) => theme.fonts.title18pt};
-`;
+  ${({ theme }) => theme.fonts.title14pt};
 
-const StyledMyInfoItemConainer = styled.section`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-
-  width: 100%;
-  padding: 2rem;
-  margin-top: 2rem;
-
-  background-color: ${({ theme }) => theme.colors.grey_popup};
-  border-radius: 0.8rem;
-
-  & :last-child {
-    margin-bottom: 0;
+  strong {
+    color: ${({ theme }) => theme.colors.sub1};
   }
 `;
