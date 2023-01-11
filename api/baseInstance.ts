d7a826e449db1bc1577c8dce15adf6bb05e3209d
YOOJS1205 <junsang8921@gmail.com>
2023-01-11 22:59:04 +0900

    Feat: api url 환경변수 설정

--- api/baseInstance.ts
+++ api/baseInstance.ts
@@ -3,31 +3,33 @@ import axios from 'axios';
 const AUTH_TOKEN =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjQsImlhdCI6MTY3Mjk0NTU0OSwiZXhwIjoxNzA0NDgxNTQ5fQ.jr6ivyfNxeL6P3nokedNl2_hC6qpiKfBjBUD3qkzFJ0';
 
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
 export const baseAPI = axios.create({
-  baseURL: 'https://api.just-pay.site/',
+  baseURL: `${API_URL}`,
 });
 
 export const salesPostAPI = axios.create({
-  baseURL: 'https://api.just-pay.site/salesposts',
+  baseURL: `${API_URL}/salesposts`,
   headers: {
     Authorization: `Bearer ${AUTH_TOKEN}`,
   },
 });
 
 export const suggestsAPI = axios.create({
-  baseURL: 'https://api.just-pay.site/suggests',
+  baseURL: `${API_URL}/suggests`,
   headers: {
     Authorization: `Bearer ${AUTH_TOKEN}`,
   },
 });
 
 export const userAPI = axios.create({
-  baseURL: 'https://api.just-pay.site/users',
+  baseURL: `${API_URL}/users`,
   headers: {
     Authorization: `Bearer ${AUTH_TOKEN}`,
   },
 });
 
 export const authAPI = axios.create({
-  baseURL: 'https://api.just-pay.site/auth',
+  baseURL: `${API_URL}/auth`,
 });
