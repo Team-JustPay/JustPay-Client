dcf329ed3d0d94a5e428a6c17193c7243271414a
JUNSANG YOO <89122773+YOOJS1205@users.noreply.github.com>
2023-01-13 07:14:24 +0900

    Fix: 토큰 변경

--- api/auth.ts
+++ api/auth.ts
@@ -2,7 +2,7 @@ import { authAPI } from './baseInstance';
 
 export const setUserLogin = async () => {
   const res = await authAPI.post('/login', {
-    oauthToken: 4,
+    oauthToken: 10,
     oauthTokenSecret: '상준쨩',
   });
   localStorage.setItem('accessToken', res.data.data.accessToken);
